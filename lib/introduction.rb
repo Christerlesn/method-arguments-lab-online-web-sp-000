6bb3760c01168059580d79ec774509411e7041d9
Christerle Nounoute <christerlesn@gmail.com>
2020-08-27 00:18:49 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hello, I am #{name}"
+end
+
+def introduction_with_language(name, language)
+  puts "Hello #{name}, I heard that you are an expert at using #{language}"
+end
