df560a049b501e8833cd12ba0a521323cfd53e67
Christerle Nounoute <christerlesn@gmail.com>
2020-08-27 00:21:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction(name)
-  puts "Hello, I am #{name}"
+  puts "Hi, my name is #{name}."
 end
 puts introduction("Sam")
 def introduction_with_language(name, language)
-  puts "Hello #{name}, I heard that you are an expert at using #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 puts introduction_with_language("Josh", "Javascript")
